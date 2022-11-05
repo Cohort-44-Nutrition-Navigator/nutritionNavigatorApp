2b1a8cd0290d88b12375c6d520b860f0b153bbb3
torontopubliclibra <dana.r.teagle@gmail.com>
2022-11-05 10:34:47 -0400

    added firebase auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,118 @@
-// import CSS styles
+// import styles
 import './App.css';
-// import Components
+
+// import components
 import Main from './Main';
-// import Hooks
+
+// import state functions
 import { useState } from 'react';
 
+// import Firebase functions
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+
+// App Component
 function App() {
+
+  // initial state variables
+  const [ loggedIn, setLoggedIn ] = useState(false);
+  const [ emailInput, setEmailInput ] = useState('');
+  const [ passwordInput, setPasswordInput ] = useState('');
+  const [ user, setUser ] = useState({
+    email: '',
+    ID: ''
+  }); 
+
+  // authentication from Firebase
+  const authentication = getAuth();
+
+  // account registration function
+  const handleRegister = (e) => {
+    // prevent refresh
+    e.preventDefault();
+    // create user account passing auth function and inputs to Firebase
+    createUserWithEmailAndPassword(authentication, emailInput, passwordInput)
+      // when the response comes back from Firebase
+      .then((response) => {
+        // log in
+        setLoggedIn(true);
+        // empty inputs
+        setEmailInput('');
+        setPasswordInput('');
+        // store the unique user info in state
+        setUser({
+          email: response.user.email,
+          ID: response.user.uid
+        });
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }
+
+  // account login function
+  const handleLogin = (e) => {
+    // prevent refresh
+    e.preventDefault();
+    // sign in user account passing auth function and inputs to Firebase
+    signInWithEmailAndPassword(authentication, emailInput, passwordInput)
+      // when the response comes back from Firebase
+      .then((response) => {
+        // log in
+        setLoggedIn(true);
+        // empty inputs
+        setEmailInput('');
+        setPasswordInput('');
+        // store the unique user info in state
+        setUser({
+          email: response.user.email,
+          ID: response.user.uid
+        });
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }
+
+  // account logout function
+  const handleLogout = (e) => {
+    // prevent refresh
+    e.preventDefault();
+    // log out
+    setLoggedIn(false);
+    // empty the user state
+    setUser({
+      email: '',
+      ID: ''
+    });
+  }
 
   return (
     <div className="App">
       <header>
         <h1>Nutrition Navigator</h1>
+
+        {/* login form */}
+        <form action="">
+          {
+            // if logged in
+            loggedIn
+            // show the logout button only
+            ? <button id="logout" onClick={handleLogout}>Logout</button>
+            // else show the inputs and the login and register buttons
+            : <>
+                <input type="email" name="email" id="email" value={emailInput} onInput={(e) => setEmailInput(e.target.value)}/>
+                <input type="password" name="password" id="password" value={passwordInput} onInput={(e) => setPasswordInput(e.target.value)}/>
+                <button id="login" onClick={handleLogin}>Login</button>
+                <button id="register" onClick={handleRegister}>Register</button>
+              </>
+          }
+        </form>
+
       </header>
-      <Main />
+
+      {/* pass user information and loggedIn state as props to Main component */}
+      <Main user={user} loggedIn={loggedIn} />
+
       <footer>
         <p>Built by Anjalee Benedict, Dana Teagle, and Joel Nash at Juno College in 2022.</p>
       </footer>
@@ -20,4 +120,5 @@ function App() {
   );
 }
 
+// export App component
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,74 +1,117 @@
-import { useState } from 'react';
+// import state functions
+import { useState, useEffect } from 'react';
+
+// import axios function
 import axios from 'axios';
+
+// import components
 import Favourites from './Favourites';
 
-const Main = () => {
+// Main component
+const Main = (props) => {
 
-    const [ userQuery, setUserQuery ] = useState('');
+  // initial state variable
+  const [ userQuery, setUserQuery ] = useState('');
+  const [ loggedIn, setLoggedIn ] = useState(false);
+  const [ user, setUser ] = useState({})
 
-    const search = () => {
-        console.log(userQuery)
+  // when props.loggedIn changes
+  useEffect(() => {
 
-        // nutritionix api docs (https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit)
+    // if logged in
+    if (props.loggedIn === true){
 
-        // axios call for search/instant endpoint
-            // axios({
-            //   url: 'https://trackapi.nutritionix.com/v2/search/instant',
-            //   headers: {
-            //     'x-remote-user-id': 0,
-            //     'x-app-id': 'ee0fb754',
-            //     'x-app-key': '14612cd5ce51f2bdb3034857e382ee9d'
-            //   },
-            //   params: {
-            //     query: userQuery,
-            //   }
-            // }).then((results) => {
-            //   console.log(results.data);
-            // })
+      // set login states
+      setLoggedIn(true);
+      setUser(props.user);
 
-        // axios call for search/item endpoint
-            // axios({
-            //   url: 'https://trackapi.nutritionix.com/v2/search/item',
-            //   headers: {
-            //     'x-remote-user-id': 0,
-            //     'x-app-id': 'ee0fb754',
-            //     'x-app-key': '14612cd5ce51f2bdb3034857e382ee9d'
-            //   },
-            //   params: {
-            //     'nix_item_id': 'd3ea6413efc0dacf7aafd8f9',
-            //   }
-            // }).then((results) => {
-            //   console.log(results.data);
-            // })
+    // else
+    } else {
 
-            // returns full nutrient breakdown with attr_ids (https://docs.google.com/spreadsheets/d/14ssR3_vFYrVAidDLJoio07guZM80SMR5nxdGpAX-1-A/edit#gid=0)
-
-        // axios call for natural/nutrients endpoint
-            // axios({
-            //     method: "post",
-            //     url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
-            //     headers: {
-            //         "x-app-id": "ee0fb754",
-            //         "x-app-key": "14612cd5ce51f2bdb3034857e382ee9d",
-            //         "x-remote-user-id": "0",
-            //     },
-            //     data: {
-            //         "query": "hot dog"
-            //     }
-            // }).then((response) => {
-            //     console.log(response.data.foods[0].full_nutrients);
-            // })
+      // empty login states
+      setLoggedIn(false);
+      setUser({});
     }
+  }, [props.loggedIn, props.user])
 
-    return(
-        <main>
-            <h2>Welcome to Nutrition Navigator</h2>
-            <input type="text" placeholder="Enter a query" value={userQuery} onInput={e => setUserQuery(e.target.value)} />
-            <button onClick={search}>Search</button>
-            <Favourites />
-        </main>
-    )
+  // search function
+  const search = () => {
+
+    console.log(userQuery)
+
+    // nutritionix api docs (https://docs.google.com/document/d/1_q-K-ObMTZvO0qUEAxROrN3bwMujwAN25sLHwJzliK0/edit)
+
+    // axios call for search/instant endpoint
+        axios({
+          url: 'https://trackapi.nutritionix.com/v2/search/instant',
+          headers: {
+            'x-remote-user-id': 0,
+            'x-app-id': 'ee0fb754',
+            'x-app-key': '14612cd5ce51f2bdb3034857e382ee9d'
+          },
+          params: {
+            query: userQuery,
+          }
+        }).then((results) => {
+          console.log(results.data);
+        })
+
+    // axios call for search/item endpoint
+        // axios({
+        //   url: 'https://trackapi.nutritionix.com/v2/search/item',
+        //   headers: {
+        //     'x-remote-user-id': 0,
+        //     'x-app-id': 'ee0fb754',
+        //     'x-app-key': '14612cd5ce51f2bdb3034857e382ee9d'
+        //   },
+        //   params: {
+        //     'nix_item_id': 'd3ea6413efc0dacf7aafd8f9',
+        //   }
+        // }).then((results) => {
+        //   console.log(results.data);
+        // })
+
+        // returns full nutrient breakdown with attr_ids (https://docs.google.com/spreadsheets/d/14ssR3_vFYrVAidDLJoio07guZM80SMR5nxdGpAX-1-A/edit#gid=0)
+
+    // axios call for natural/nutrients endpoint
+        // axios({
+        //     method: "post",
+        //     url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
+        //     headers: {
+        //         "x-app-id": "ee0fb754",
+        //         "x-app-key": "14612cd5ce51f2bdb3034857e382ee9d",
+        //         "x-remote-user-id": "0",
+        //     },
+        //     data: {
+        //         "query": "hot dog"
+        //     }
+        // }).then((response) => {
+        //     console.log(response.data.foods[0].full_nutrients);
+        // })
+  }
+
+  // Main component return
+  return(
+      <main>
+
+          {/* search input */}
+          <input type="text" placeholder="Enter a query" value={userQuery} onInput={e => setUserQuery(e.target.value)} />
+          <button onClick={search}>Search</button>
+
+          {
+              // if logged in
+              loggedIn
+              // display the user id
+              ? <>
+                  <p>Welcome to the page, {user.email}. Your unique ID is {user.ID}.</p>
+                  <Favourites ID={user.ID} />
+                </>
+              : <p>Please log in.</p>
+          }
+      </main>
+  )
 
 }
 
+// export Main component
 export default Main;
